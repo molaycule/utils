c561c5f55e9c321147aa60d1a52e5737c57b6888
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-04-05 10:00:29 +0100

    Create NestedObjectFlattener.js

---
+++ NestedObjectFlattener.js
@@ -0,0 +1,53 @@
+// flatten nested object
+// link to replit: https://replit.com/talk/share/Nested-Object-Flattener/133000
+let flattenNestedObject = (obj, key = null) => {
+  return Object.keys(obj).reduce((acc, cur) => {
+    let nestedKey = key ? `${key}.${cur}` : cur;
+    
+    if (typeof obj[cur] === "object") {
+      return {...acc, ...flattenNestedObject(obj[cur],  nestedKey)};
+    }
+
+    return {...acc, [nestedKey]: obj[cur]};
+  }, {});
+};
+
+// Example
+let obj = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 4,
+    e: 5,
+    f: {
+      g: 7,
+      h: 8,
+      i: {
+        j: 10,
+        k: 11,
+        l: {
+          m: 13,
+          n: 14,
+          o: {
+            p: 16, 
+            q: 17,
+            r: {
+              s: 19,
+              t: 20,
+              u: {
+                v: 22,
+                w: 23,
+                x: {
+                  y: 25,
+                  z: 26
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+console.log(flattenNestedObject(obj));
