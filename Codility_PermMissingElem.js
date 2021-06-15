6fbf759edbd0480113ff54357e85a9502ceb57a3
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-06-15 18:30:21 +0100

    task: delete file

--- Codility_PermMissingElem.js
+++
@@ -1,9 +0,0 @@
-function solution(A) {
-    let arr = Array(A.length + 1);
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i] || 0;
-        let item = A[i];
-        if (item) arr[item - 1] = item;
-    }
-    return arr.indexOf(0) + 1
-}
