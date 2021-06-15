313be24f66085b77a76c1a5f3100f31e85a1ef1e
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-06-15 18:27:05 +0100

    task: delete file

--- Codility_Distinct.js
+++
@@ -1,8 +0,0 @@
-function solution(A) {
-    let distinct = {};
-    for (let item of A) {
-        if (distinct[item]) continue;
-        distinct[item] = 1;
-    }
-    return Object.keys(distinct).length;
-}
