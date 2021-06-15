3628f51349240b40219e96bfe9470099f1f6b936
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-06-15 13:08:36 +0100

    task: solution for Codility_Dominator.js format file

--- Codility_Dominator.js
+++ Codility_Dominator.js
@@ -1,13 +1,13 @@
 function solution(A) {
-  let halfSize = A.length / 2, counterObj = {}, max = 0, result = -1;
-  for (let i = 0; i < A.length; i++) {
-      counterObj[A[i]] = counterObj[A[i]] + 1 || 1;
-      if (counterObj[A[i]] > max) {
-          max = counterObj[A[i]];
-          result = i;
-      }
-      if (max > halfSize) break;
-      else result = -1;
-  }
-  return result;
+    let halfSize = A.length / 2, counterObj = {}, max = 0, result = -1;
+    for (let i = 0; i < A.length; i++) {
+        counterObj[A[i]] = counterObj[A[i]] + 1 || 1;
+        if (counterObj[A[i]] > max) {
+            max = counterObj[A[i]];
+            result = i;
+        }
+        if (max > halfSize) break;
+        else result = -1;
+    }
+    return result;
 }
