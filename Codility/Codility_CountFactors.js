c856e02076d6a5932320dd6e057eae67dc3f38b0
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-06-18 09:17:55 +0100

    task: updated solution for Codility_CountFactors.js

--- Codility/Codility_CountFactors.js
+++ Codility/Codility_CountFactors.js
@@ -1,6 +1,6 @@
 function solution(N) {
     let factors = new Set();
-    for (let i = 0; i <= Math.sqrt(N); i++) {
+    for (let i = 1; i <= Math.sqrt(N); i++) {
         if (N % i === 0) {
             factors.add(i);
             factors.add(N / i);
