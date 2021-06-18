0b801fcb239bfee101885334dfbe10b0e848d14d
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-06-18 22:43:09 +0100

    task: solution for Codility_MaxSliceSum.js

---
+++ Codility/Codility_MaxSliceSum.js
@@ -0,0 +1,11 @@
+function solution(A) {
+  let maxEnding = maxSliceSum = 0;
+  A.forEach((item, index) => {
+      if (index === 0) maxEnding = maxSliceSum = item;
+      else {
+          maxEnding = Math.max(maxEnding + item, item);
+          maxSliceSum = Math.max(maxSliceSum, maxEnding);
+      }
+  });
+  return maxSliceSum;
+}
