61023b7a84a0093004e4fdae5d1bc8e422c46712
Mohammed Agboola®️ <moh.agboola@gmail.com>
2022-06-03 22:33:02 +0100

    Create function-overloading.ts

---
+++ function-overloading.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return a + b;
+  }
+}
+
+// console.log(add(1, 2));
+// console.log(add('1', '2'));
